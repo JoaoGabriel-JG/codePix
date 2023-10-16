bbdda38c927a56c40d2d4a9672e1a190f542231b
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2023-10-16 18:50:28 -0300

    start

---
+++ domain/model/bank.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+type Bank struct {
+	ID        string    `json:"id"`
+	Code      string    `json:"code"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.ID = uuid.NewV4().String()
+
+	return &bank, nil
+}
